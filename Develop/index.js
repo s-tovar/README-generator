f3daebfe02f8b8d1d47e1c5fd992906c28faee87
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-05-15 00:56:39 -0700

    created array of questions for user input in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description for your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instrcution for your project:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information for the project:',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution information for the project:',
+      },
+      { 
+        type: 'input',
+        name: 'tests',
+        message: 'Provide testing instructions for the project:',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Provide license information for the project:',
+      },
+      { 
+        type: 'input',
+        name: 'questions',
+        message: 'Provide contact information for questions:',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
